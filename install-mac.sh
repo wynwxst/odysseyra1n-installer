4147c180801ad46c01cd50526dd9e163378df78a
Cool5tar <cool5tarXV@gmail.com>
2020-08-16 17:50:59 +0800

    Create install-mac.sh

---
+++ install-mac.sh
@@ -0,0 +1,7 @@
+echo "run as root"
+
+echo "Cool5tar's odysseyra1n installer starting..."
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)”
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/cool5tar/odysseyra1n-installer/master/installer.sh)"
